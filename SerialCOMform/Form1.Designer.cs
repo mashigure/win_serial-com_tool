3664abefe9968c4d3b9a4ff83cf917f1704c6179
mashigure <mashigure@live.jp>
2015-08-16 15:01:45 +0200

    first commit

---
+++ SerialCOMform/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace SerialCOMform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.SendText = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ReceiveTextBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.BaudComboBox = new System.Windows.Forms.ComboBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.COMcomboBox = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SendButton
+            // 
+            this.SendButton.Enabled = false;
+            this.SendButton.Location = new System.Drawing.Point(651, 3);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(106, 38);
+            this.SendButton.TabIndex = 1;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // SendText
+            // 
+            this.SendText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SendText.Location = new System.Drawing.Point(3, 3);
+            this.SendText.Name = "SendText";
+            this.SendText.Size = new System.Drawing.Size(642, 31);
+            this.SendText.TabIndex = 2;
+            this.SendText.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SendText_KeyDown);
+            this.SendText.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SendText_KeyPress);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel1.Controls.Add(this.SendButton, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.SendText, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(768, 49);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.ReceiveTextBox, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel4, 0, 3);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 5;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 56F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 56F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(774, 529);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 3;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 180F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 180F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 404);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(768, 26);
+            this.tableLayoutPanel3.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Location = new System.Drawing.Point(411, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Baud rate:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label2.Location = new System.Drawing.Point(3, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(402, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "COM:";
+            // 
+            // ReceiveTextBox
+            // 
+            this.ReceiveTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.ReceiveTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ReceiveTextBox.Location = new System.Drawing.Point(3, 59);
+            this.ReceiveTextBox.Multiline = true;
+            this.ReceiveTextBox.Name = "ReceiveTextBox";
+            this.ReceiveTextBox.ReadOnly = true;
+            this.ReceiveTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.ReceiveTextBox.Size = new System.Drawing.Size(768, 339);
+            this.ReceiveTextBox.TabIndex = 4;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 3;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 180F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 180F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel4.Controls.Add(this.BaudComboBox, 1, 0);
+            this.tableLayoutPanel4.Controls.Add(this.ConnectButton, 2, 0);
+            this.tableLayoutPanel4.Controls.Add(this.COMcomboBox, 0, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(3, 436);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(768, 50);
+            this.tableLayoutPanel4.TabIndex = 7;
+            // 
+            // BaudComboBox
+            // 
+            this.BaudComboBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BaudComboBox.FormattingEnabled = true;
+            this.BaudComboBox.Items.AddRange(new object[] {
+            "300",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "28800",
+            "31250",
+            "38400",
+            "57600",
+            "115200 "});
+            this.BaudComboBox.Location = new System.Drawing.Point(411, 3);
+            this.BaudComboBox.Name = "BaudComboBox";
+            this.BaudComboBox.Size = new System.Drawing.Size(174, 32);
+            this.BaudComboBox.TabIndex = 1;
+            this.BaudComboBox.Text = "9600";
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ConnectButton.Location = new System.Drawing.Point(591, 3);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(174, 44);
+            this.ConnectButton.TabIndex = 0;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // COMcomboBox
+            // 
+            this.COMcomboBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.COMcomboBox.FormattingEnabled = true;
+            this.COMcomboBox.Location = new System.Drawing.Point(3, 3);
+            this.COMcomboBox.Name = "COMcomboBox";
+            this.COMcomboBox.Size = new System.Drawing.Size(402, 32);
+            this.COMcomboBox.TabIndex = 2;
+            this.COMcomboBox.DropDown += new System.EventHandler(this.COMcomboBox_DropDown);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 494);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(774, 35);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(182, 30);
+            this.toolStripStatusLabel1.Text = "Not connected";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 529);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Name = "Form1";
+            this.Text = "Serial COM";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.TextBox SendText;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox ReceiveTextBox;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.ComboBox BaudComboBox;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox COMcomboBox;
+    }
+}
+
---
+++ SerialCOMform/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SerialCOMform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            COMcomboBox.Items.AddRange( SerialPort.GetPortNames() );
+            if( 0 < COMcomboBox.Items.Count)
+            {
+                COMcomboBox.Text = COMcomboBox.Items[0].ToString();
+            }
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            sendSerial();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if( serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                    ConnectButton.Text = "Connect";
+                    SendButton.Enabled = false;
+                    toolStripStatusLabel1.Text = "Not connected";
+                }
+                else
+                {
+                    serialPort1.PortName = COMcomboBox.Text;
+                    serialPort1.BaudRate = Int32.Parse( BaudComboBox.Text );
+                    serialPort1.Open();
+                    ConnectButton.Text = "Disconnect";
+                    SendButton.Enabled = true;
+                    toolStripStatusLabel1.Text = "Connected (" + COMcomboBox.Text + ")";
+                }
+            }
+            catch {
+                toolStripStatusLabel1.Text = "Error";
+            }
+        }
+
+        private void COMcomboBox_DropDown(object sender, EventArgs e)
+        {
+            COMcomboBox.Items.Clear();
+            COMcomboBox.Items.AddRange(SerialPort.GetPortNames());
+        }
+
+        private delegate void printRecievedStrDelegate(string data);
+
+        private void printRecievedStr(string data)
+        {
+            ReceiveTextBox.Text += data;
+
+            // 自動スクロール
+            ReceiveTextBox.SelectionStart = ReceiveTextBox.Text.Length;
+            ReceiveTextBox.Focus();
+            ReceiveTextBox.ScrollToCaret();
+            SendText.Focus();
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string received_str = serialPort1.ReadExisting();
+            printRecievedStrDelegate print = new printRecievedStrDelegate(printRecievedStr);
+            ReceiveTextBox.Invoke(print, received_str);
+        }
+
+        private void SendText_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                sendSerial();
+            }
+        }
+
+        private void sendSerial()
+        {
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Write(SendText.Text);
+                    SendText.Text = "";
+                }
+                catch
+                {
+                    toolStripStatusLabel1.Text = "Send Error";
+                }
+            }
+        }
+
+        private void SendText_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // BEEP抑制
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
